3d9bb1f7dd6fe148b200d13f906a3241e47fd378
BikerViking <fredovelha@gmail.com>
2017-12-07 11:04:01 -0300

    Update Main.py

--- Code/Main.py
+++ Code/Main.py
@@ -10,9 +10,9 @@ This game is a project that I've startet in my free time while I was studying pr
 
 The game is going to be a simple Roguelike*.
 
-The main mechanic of my game is to make death meaninful by tracking the attempts to reach the bottom of the dungeon, 
-so in the next playthrough and on and player can come across the dead heroes which can take various forms; 
-from a dead corpse that players can loot for itens, to minibosses.
+The main mechanic of my game is to make death meaningful by tracking the attempts to reach the bottom of the dungeon, 
+so in the next playthrough and on, the player can come across the dead heroes which can take various forms; 
+from a dead corpse that players can loot for items, to minibosses.
 
 By doing so, players are filling the dungeon with dead characters, making their own catacombs - which is the name of the game.
 
