5da4274c4daf2af9ca2d3d776f4058ad62d2b7fa
BikerViking <fredovelha@gmail.com>
2017-12-07 10:31:46 -0300

    Update Main.py

--- Code/Main.py
+++ Code/Main.py
@@ -4,65 +4,18 @@ if __name__ == "__main__":
 	Core = Core.Core()
 
 """
-Hi, I'm Frederico Dargel, AKA, BikerViking!
+Hi, I'm Frederico Dargel.
 
-This game is a project that I've started in my free time while I was
-studiyng programing by myself
+This game is a project that I've startet in my free time while I was studying programing by myself.
 
-The game main concept is rougelike, a game where everytime you play the
-map will be randomly generated.
-So map layout, monsters, loot, will be random.
-Roguelikes also have permadeath, so once you die that's it. You'll need
-to start over from the beginning.
+The game is going to be a simple Roguelike*.
 
-My game is called Catacombs - and the game's name kinda spoils the main
-idea behind it.
+The main mechanic of my game is to make death meaninful by tracking the attempts to reach the bottom of the dungeon, 
+so in the next playthrough and on and player can come across the dead heroes which can take various forms; 
+from a dead corpse that players can loot for itens, to minibosses.
 
-The idea is to make your hero's death meaninfull for the next
-playthrough and on.
-So, when you die, the game's algorthim will decide what really happens
-to that Hero.
-In your next playthrough and on, will be able to find your dead heroes
-inside the dungeon.
-Sometimes the dead hero it's just a corpse that you can loot
-but the dead hero can have multiple fates such as: a hopeless soul
-seeking something, or a undead friendly npc that will sell itens, or
-even be a minibosses
+By doing so, players are filling the dungeon with dead characters, making their own catacombs - which is the name of the game.
 
-And the end of the dungeon you'll find the dungeon soul, a been that
-represents the dungeon itself.
-It will tell the hero that he is actually a bad guy. He invaded his
-domain, killed inocent monsters, raided the place.
-But It's not like he is mad or something. It knows the human condition.
-Humans, in It's point of view, are exploeres and conquerers. And their
-ethical values are very questionable, contradictory, selfish and
-hypocritical.
-By sharing his point of view, It makes the hero an offer:
-
-The hero can either go away with everything that his body and mind can
-barer
-or the hero can fight the dungeon to put a end on this story.
-
-IF the player chooses to go away, he'll need to make his way out of the
-dungeon. He'll see each and every creature that he murdered and the
-ones that are still alive now run away. The dungeon traps will also be
-deactivated.
-When the hero reaches the exit he'll drop all his belongings, write a
-letter and jump off a cliff.
-
-The next playthrough after this one starts with the aged gear, but it's 
-a little bit stronger than starter gear, of the previous hero and his 
-suicide letter.
-
-BUT IF the player chooses to fight, and wins, the dungeon corpse will 
-reveal a big soul which will be absorved by the hero.
-That will reset the dead heroes from the dungeon and unlock new itens,
-monsters and a few more floors to the next playthrough.
-
-You probalby felt that there is a meaning behind it all. And there
-really is. It's going to be fun to see all the theories and interpretations
-of the game's lore.
-
-I hope that you enjoy the concept and my game, once I finish it.
-Consider donate if you do.
+"Roguelike is a subgenre of role-playing video games, characterized by a dungeon crawl through procedurally generated game levels, 
+turn-based gameplay, tile-based graphics, and permanent death of the player-character." - Wikipedia.
 """
