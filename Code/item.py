b05923943a7b1de9107f9b58333f0485db7844c3
BikerViking <frederico.dargel@gmail.com>
2018-03-11 00:25:51 -0300

    removing old code

--- Code/item.py
+++
@@ -1,6 +0,0 @@
-import agent
-
-class Item(agent.Agent):
-	"""docstring for Item"""
-	def __init__(self):
-		super(Item, self).__init__()
--- Code/player.py
+++
@@ -1,6 +0,0 @@
-import creature
-
-class Hero(creature.Creature):
-	"""docstring for Creature"""
-	def __init__(self):
-		super(Hero, self).__init__()
